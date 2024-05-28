784e2330eb081c085259f3c3b51ef5ae3068f3c9
septinna <129613418+septinna@users.noreply.github.com>
2024-05-28 09:51:46 +0700

    clear repo

--- src/StyledFooter.js
+++
@@ -1,9 +0,0 @@
-// StyledFooter.js
-import styled from 'styled-components';
-
-export const StyledFooter = styled.footer`
-    background-color: #333;
-    color: #fff;
-    padding: 20px 0;
-    text-align: center;
-`;
