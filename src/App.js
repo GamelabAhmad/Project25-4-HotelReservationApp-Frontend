bf8ea58d539305b885e1a58c052647c6f4d74f48
Fazayoga <fazayoga23@gmail.com>
2024-05-28 10:44:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './page/Header';
 import Footer from './page/Footer';
 import Kontak from './page/Kontak';
@@ -9,30 +10,37 @@ import Room from './page/Room';
 import Login from './page/Login';
 import Daftar from './page/Daftar';
 import DetailRoom from './page/DetailRoom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './css/bootstrap.min.css';
 import './css/animate.css';
 
 const App = () => {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Body />} /> {/* Temporary Home component */}
-            <Route path="/tentang" element={<Tentang />} />
-            <Route path="/ruang" element={<Room />} />
-            <Route path="/kontak" element={<Kontak />} />
-            <Route path="/detail-kamar" element={<DetailRoom />} />
-            <Route path="/masuk" element={<Login />} />
-            <Route path="/daftar" element={<Daftar />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
-    );
-}
+  const location = useLocation();
+  const noHeaderFooterPaths = ['/masuk', '/daftar'];
+  const shouldHideHeaderFooter = noHeaderFooterPaths.includes(location.pathname);
 
-export default App;
+  return (
+    <div className="App">
+      {!shouldHideHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/tentang" element={<Tentang />} />
+        <Route path="/ruang" element={<Room />} />
+        <Route path="/kontak" element={<Kontak />} />
+        <Route path="/detail-kamar" element={<DetailRoom />} />
+        <Route path="/masuk" element={<Login />} />
+        <Route path="/daftar" element={<Daftar />} />
+      </Routes>
+      {!shouldHideHeaderFooter && <Footer />}
+    </div>
+  );
+};
+
+const WrappedApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default WrappedApp;
