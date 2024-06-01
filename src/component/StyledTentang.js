869096ed1ddc50d447c2f128b314aad611428786
septinna <129613418+septinna@users.noreply.github.com>
2024-06-02 03:32:04 +0700

    update

--- src/component/StyledTentang.js
+++ src/component/StyledTentang.js
@@ -1,35 +1,39 @@
 import styled from 'styled-components';
 
 export const Main = styled.main`
-  /* CSS untuk Main */
+  padding: 20px;
+  font-family: Arial, sans-serif;
 `;
 
 export const AboutSection = styled.section`
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start; 
-  padding: 30px;
+  margin-bottom: 40px;
 `;
 
 export const Container = styled.div`
   /* CSS untuk Container */
   display: flex;
-  align-items: space-between;
+  flex-direction: row; /* Mengatur agar elemen dalam container diatur secara horizontal */
+  align-items: flex-start;
+  justify-content: space-between;
   max-width: 1600px;
   margin: 0 auto;
   padding: 0 20px;
 `;
 
 export const Column = styled.div`
-  /* CSS untuk Column */
   flex: 1;
-  margin-right: 20px;
+  margin: 40px 60px 0 20px; /* Add margin to top and both sides for better spacing */
+  text-align: justify; /* Justify the text in the column */
+  width: 50%; /* Memberikan lebar agar kedua kolom tidak bertumpuk */
+  font-size: 1.5rem;
 `;
 
 export const Image = styled.img`
-  /* CSS untuk Image */
-  max-width: 100%;
-  height: auto;
+  width: 100%; /* Lebar gambar 100% dari elemen pembungkus */
+  max-width: 800px; /* Lebar maksimum gambar */
+  height: 300px; /* Tinggi gambar */
+  object-fit: contain; /* Menjaga proporsi gambar */
+  margin-left: 20px; /* Posisikan gambar lebih ke kanan dengan margin kiri */
 `;
 
 export const TeamSection = styled.section`
